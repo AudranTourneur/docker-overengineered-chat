46072e51d148357f9afd729785aa74204952e4ad
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2023-12-02 18:48:45 +0100

    initial commit

---
+++ back/app/Models/Session.ts
@@ -0,0 +1,17 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+
+export default class Session extends BaseModel {
+  @column({ isPrimary: true })
+  public token: string
+
+  @hasOne(() => User)
+  public user: HasOne<typeof User>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
